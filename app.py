d2c14121e4f848ee0449302b5f0d24d999cea2fa
Likibi <likibikangben@gmail.com>
2020-09-16 10:55:54 +0000

    Added templates

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask
+import os
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'Blog_manager'
+app.config["MONGO_URI"] = 'mongodb+srv://root:G48iN3Fsv849ZF@myfirstcluster.geudv.mongodb.net/Blog_manager?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+@app.route('/get_movies')
+def get_tasks():
+    return render_template("tasks.html", movies=mongo.db.movies.find())
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
