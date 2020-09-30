9cf92df4f79f94e62fe3c00ca39ef927a9c4b286
Likibi <likibikangben@gmail.com>
2020-09-30 11:25:43 +0000

    Fixing bugs for atlas

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'blog'
 app.config["MONGO_URI"] = 'mongodb+srv://root:Diamond12@myfirstcluster.geudv.mongodb.net/blog?retryWrites=true&w=majority'
-
 mongo = PyMongo(app)
 
 @app.route('/')
